ba313460ea8902e23776828677a41544ab2481ef
acoustic_jeet <js.jitendra11@gmail.com>
2018-05-21 04:21:39 +0530

    Update checkPrime.cpp

--- checkPrime.cpp
+++ checkPrime.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 bool primeCheck(int n){
+        if(n <= 1)
+                return false;
         for(int i = 2; i*i <= n; i++){
                 if(n % i == 0)
                         return false;
